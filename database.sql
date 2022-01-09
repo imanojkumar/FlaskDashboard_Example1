2b1784f9fe62963553f2a9a2a58f4699526e1650
m kumar <ekumarmanoj@gmail.com>
2022-01-09 13:22:49 +0530

    initial commit
    initial commit

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS flasklogin DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE flasklogin;
+
+CREATE TABLE IF NOT EXISTS accounts (
+	id int(11) NOT NULL AUTO_INCREMENT,
+  	username varchar(50) NOT NULL,
+  	password varchar(255) NOT NULL,
+  	email varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO accounts (id, username, password, email) VALUES (1, 'test', 'test', 'test@test.com');
+INSERT INTO accounts (id, username, password, email) VALUES (2, 'manu', 'manu123', 'manu@test.com');
